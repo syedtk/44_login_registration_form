f056a73f9a387904b2ba240bce2c5a284d5f9713
01723710909 <137289906+01723710909@users.noreply.github.com>
2024-01-10 18:26:56 +0600

    7

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -5,7 +5,7 @@ import ShopProduct from './Product/ShopProduct';
 const Shop = () => {
     const products = useLoaderData();
 
-
+ 
     const renderedProducts = Array.isArray(products)
         ? products.map(product => (
             <ShopProduct
@@ -14,23 +14,34 @@ const Shop = () => {
             />
         ))
         : null;
-    
     return (
         <div className='flex container mx-auto my-20'>
             <div className='sm:w-4/5 md:px-6'>
-                <div className="my-12 container mx-auto grid sm:grid-cols-2 md:grid-cols-4 gap-3">
-                    {renderedProducts}
+                <div className=" my-12 container mx-auto grid sm:grid-cols-2 md:grid-cols-4 gap-3">
+                {
+                   products.map(product=>
+                    <ShopProduct
+                     key={product.id}
+                     product={product}> 
+                    </ShopProduct>)
+                }
                 </div>
             </div>
 
-            <div className='leading-[2.2rem] mt-4 bg-yellow-300 sm:w-1/5 container mx-auto p-3 fixed top-[110px] right-3 rounded-2xl'>
-                <h1 className='text-2xl text-center mt-4'>Order Summary</h1>
+
+
+            <div className='leading-[2.2rem] mt-4 bg-yellow-300 sm:w-1/5 conatainer mx-auto p-3 fixed top-[110px] right-3 rounded-2xl'>
+                <h1 className='text-2xl text-center mt-4'>Order Summery</h1>
                 <div className='mb-6'>
-                    {/* Your order summary details here */}
+                    <p>Selected Items: 6</p>
+                    <p>Total Price: $111</p>
+                    <p>Total Shiping Charge</p>
+                    <p>Tax: 11</p>
+                    <p>Grand Total: $122</p>
+                    <button className='btn btn-sm w-full hover:text-black bg-red-500 border-0 my-3 text-white'>Clear Cart</button> <br/>
+                    <button className='btn btn-sm w-full hover:text-black bg-orange-500 border-0 text-white'>Review Order</button>
                 </div>
-                <button className='btn btn-sm w-full hover:text-black bg-red-500 border-0 my-3 text-white'>Clear Cart</button>
-                <br />
-                <button className='btn btn-sm w-full hover:text-black bg-orange-500 border-0 text-white'>Review Order</button>
+                 
             </div>
         </div>
     );
